79fd801ee89bf3b1c20266ebd8d49b6a429fbd7b
Lucas-Mello851 <145824124+Lucas-Mello851@users.noreply.github.com>
2026-04-03 20:02:18 -0300

    Criando a primeira rota
    criação da rota principal para renderizar interface do chatbot

--- chatbot/app.py
+++ chatbot/app.py
@@ -3,3 +3,7 @@ from chatbot.service import processar_mensagem
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = "chatbot_consumidor_secret_key_2026"
+
+@app.route("/")
+def index():
+    return render_template("index.html")

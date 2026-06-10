488fe4068aa01e97f80a485677c1b32c22541616
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-09 22:44:04 -0300

    Removendo funções auxiliares de consulta ao histórico

--- chatbot/memory.py
+++ chatbot/memory.py
@@ -94,38 +94,6 @@ def detectar_entidades(texto):
             ents["empresa"] = emp.title()
             break
 
-def obter_ultima_intencao(historico):
-    if not historico:
-        return None
-    return historico[-1].get("intencao")
-
-
-def obter_ultima_mensagem_usuario(historico):
-    if not historico:
-        return None
-    return historico[-1].get("usuario")
-
-
-def obter_ultimas_intencoes(historico, limite=5):
-    return [item.get("intencao") for item in historico[-limite:]]
-
-
-def contar_intencao_no_historico(historico, intencao):
-    return sum(1 for item in historico if item.get("intencao") == intencao)
-
-
-def obter_temas_visitados(historico):
-    vistos = []
-    for item in historico:
-        tag = item.get("intencao")
-        if tag and tag not in ("desconhecida", "saudacao", "despedida", "agradecimento") and tag not in vistos:
-            vistos.append(tag)
-    return vistos
-
-
-def obter_num_interacoes(historico):
-    return len(historico)
-
 
 def detectar_tom_usuario(mensagem):
     msg_lower = mensagem.lower()

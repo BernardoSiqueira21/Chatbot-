4979e0e57098e07879edfeec3a0a498e09e03b68
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-09 22:39:34 -0300

    Mapeando meses para reconhecimento de datas

--- chatbot/memory.py
+++ chatbot/memory.py
@@ -41,6 +41,10 @@ _CANAIS = {
     "delivery":  ["delivery","entrega","ifood","rappi","por entrega"],
 }
 
+_MESES = {
+    "janeiro":1,"fevereiro":2,"marco":3,"abril":4,"maio":5,"junho":6,
+    "julho":7,"agosto":8,"setembro":9,"outubro":10,"novembro":11,"dezembro":12,
+}
 
 
 def obter_ultima_intencao(historico):
